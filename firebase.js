a2f1ba61b6a547a883b618081eb9f7560296e4d8
cramsen <johnmarcvidanes@gmail.com>
2026-07-12 16:36:46 +0800

    Switch to environment variable for Firebase credentials

--- firebase.js
+++ firebase.js
@@ -1,11 +1,8 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getDatabase } from 'firebase-admin/database';
-import fs from 'node:fs';
 
-// Bulletproof JSON loading for ES Modules
-const serviceAccount = JSON.parse(
-    fs.readFileSync(new URL('./serviceAccountKey.json', import.meta.url), 'utf8')
-);
+// Load credentials from environment variable instead of a local file
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 const app = initializeApp({
   credential: cert(serviceAccount),
